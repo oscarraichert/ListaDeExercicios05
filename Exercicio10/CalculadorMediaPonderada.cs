fe79381ea1f581f03cb623b6ac4cf3828382f6da
oscar <oscarraichert@hotmail.com>
2022-03-13 21:05:22 -0300

    exercicio10

---
+++ Exercicio10/CalculadorMediaPonderada.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercicio10
+{
+    internal partial class Program
+    {
+        public class CalculadorMediaPonderada
+        {
+            public decimal[] Valores;
+            public decimal Media;
+
+            public CalculadorMediaPonderada(decimal[] valores)
+            {
+                Valores = valores;
+            }
+
+            public override string ToString()
+            {
+                return $"A média ponderada das notas é: {Media}";
+            }
+
+            public decimal CalcularMediaPonderada()
+            {
+                decimal valorAtual = 0;
+                for (int i = 0; i < Valores.Length; i++)
+                {
+                    valorAtual += Valores[i];
+                }
+
+                Media = valorAtual / Valores.Length;
+                return Media;
+            }
+        }
+    }
+}
---
+++ Exercicio10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercicio10
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculadorMediaPonderada calculadora = InserirDados();
+            calculadora.CalcularMediaPonderada();
+            Console.WriteLine(calculadora);
+        }
+
+        public static CalculadorMediaPonderada InserirDados()
+        {
+            decimal[] valores = new decimal[4];
+
+            Console.WriteLine("Insira as notas para obter a média ponderada: \n");
+
+            for (int i = 0; i < valores.Length; i++)
+            {
+                Console.Write($"Nota {i + 1}: ");
+                valores[i] = Convert.ToDecimal(Console.ReadLine());
+            }
+
+            CalculadorMediaPonderada calculador = new CalculadorMediaPonderada(valores);
+
+            return calculador;
+        }
+    }
+}
